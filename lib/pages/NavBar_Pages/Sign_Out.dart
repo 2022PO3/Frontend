9b50beb34daf30a67adb668fd44028f04d0e2153
Rubenm03 <ruben.marien@student.kuleuven.be>
2022-11-04 16:31:23 +0100

    Sign out page

---
+++ lib/pages/NavBar_Pages/Sign_Out.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+class Sign_Out extends StatefulWidget {
+  const Sign_Out({Key? key}) : super(key: key);
+
+  @override
+  State<Sign_Out> createState() => _Sign_OutState();
+}
+
+class _Sign_OutState extends State<Sign_Out> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //
+      ),
+      body: Text("Sign_Out"),
+    );
+  }
+}
