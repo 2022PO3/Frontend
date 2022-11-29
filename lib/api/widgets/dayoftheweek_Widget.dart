db28f31ce64d39d260988dcfe606d0052aa06eae
Rubenm03 <ruben.marien@student.kuleuven.be>
2022-11-29 17:46:13 +0100

    test

--- lib/api/widgets/dayoftheweek_Widget.dart
+++ lib/api/widgets/dayoftheweek_Widget.dart
@@ -1,2 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:po_frontend/api/models/opening_hour_model.dart';
+class Dayoftheweek extends StatefulWidget {
+  const Dayoftheweek({Key? key}) : super(key: key);
+
+  @override
+  State<Dayoftheweek> createState() => _DayoftheweekState();
+}
+
+class _DayoftheweekState extends State<Dayoftheweek> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
