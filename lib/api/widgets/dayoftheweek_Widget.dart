51e9f7f531f47234282537cfcde10a96ff0f892b
Rubenm03 <ruben.marien@student.kuleuven.be>
2022-11-29 17:58:44 +0100

    Garage info

--- lib/api/widgets/dayoftheweek_Widget.dart
+++ lib/api/widgets/dayoftheweek_Widget.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:po_frontend/api/models/opening_hour_model.dart';
-class Dayoftheweek extends StatefulWidget {
-  const Dayoftheweek({Key? key}) : super(key: key);
+class Dayoftheweek extends StatelessWidget {
+  const Dayoftheweek({Key? key, required this.openingshours}) : super(key: key);
+  final OpeningHour openingshours;
 
-  @override
-  State<Dayoftheweek> createState() => _DayoftheweekState();
-}
-
-class _DayoftheweekState extends State<Dayoftheweek> {
   @override
   Widget build(BuildContext context) {
     return Container();
   }
 }
+
