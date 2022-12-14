e7f92b5e94038dbfe404b567a0085db4c04c428a
Robin Martens <robin.martens1@student.kuleuven.be>
2022-12-14 17:56:42 +0100

    Beautified new reservation page

--- lib/utils/button.dart
+++ lib/utils/button.dart
@@ -4,37 +4,46 @@ import 'package:po_frontend/utils/constants.dart';
 Widget buildButton(
   String text,
   Color color,
+  Function onPressed, {
+  bool onlyExpanded = false,
+}) {
+  return onlyExpanded
+      ? _buildButton(text, color, onPressed)
+      : Row(
+          children: [_buildButton(text, color, onPressed)],
+        );
+}
+
+Widget _buildButton(
+  String text,
+  Color color,
   Function onPressed,
 ) {
-  return Row(
-    children: [
-      Expanded(
-        child: OutlinedButton(
-          style: OutlinedButton.styleFrom(
-            backgroundColor: color,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(
-                Constants.borderRadius,
-              ),
-            ),
-          ),
-          onPressed: () {
-            onPressed();
-          },
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 15,
-            ),
-            child: Text(
-              text,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-              ),
-            ),
+  return Expanded(
+    child: OutlinedButton(
+      style: OutlinedButton.styleFrom(
+        backgroundColor: color,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+            Constants.borderRadius,
           ),
         ),
       ),
-    ],
+      onPressed: () {
+        onPressed();
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 15,
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          ),
+        ),
+      ),
+    ),
   );
 }
--- lib/utils/card.dart
+++ lib/utils/card.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:po_frontend/utils/constants.dart';
 
-Widget buildCard({required List<Widget> children}) {
+Widget buildCard({
+  required List<Widget> children,
+  bool expanded = false,
+}) {
+  return expanded
+      ? Row(
+          children: [
+            Expanded(child: _buildCard(children: children)),
+          ],
+        )
+      : _buildCard(children: children);
+}
+
+Widget _buildCard({required List<Widget> children}) {
   return Card(
     shape: Constants.cardBorder,
     child: Padding(
