b801364e8c51f3fa42af37efa7cc823e1d9beb70
Rune2003 <rune.verachtert@telenet.be>
2022-12-15 12:04:00 +0100

    Fix overflow errors

--- lib/api/widgets/garage_widget.dart
+++ lib/api/widgets/garage_widget.dart
@@ -17,23 +17,26 @@ class GarageWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final double height = MediaQuery.of(context).size.longestSide;
-
     return Card(
       shape: Constants.cardBorder,
       child: InkWell(
         child: Row(
           children: [
-            buildFreeSpotsBanner(
-              context,
-              garage,
-              height,
-              Constants.borderRadius,
+            Expanded(
+              child: buildFreeSpotsBanner(
+                context,
+                garage,
+                //height,
+                Constants.borderRadius,
+              ),
             ),
-            buildGarageInformationBanner(
-              context,
-              garage,
-              height,
+            Expanded(
+              flex: 4,
+              child: buildGarageInformationBanner(
+                context,
+                garage,
+                // height,
+              ),
             ),
           ],
         ),
@@ -47,71 +50,71 @@ class GarageWidget extends StatelessWidget {
   Widget buildFreeSpotsBanner(
     BuildContext context,
     Garage garage,
-    double height,
     double borderRadius,
   ) {
-    final double width = MediaQuery.of(context).size.shortestSide;
     final double maxSpots = garage.parkingLots.toDouble();
     final double unoccupiedLots = garage.unoccupiedLots.toDouble();
     final bool full = maxSpots == unoccupiedLots;
 
-    return Container(
-      width: width / 6,
-      height: height / 11,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.horizontal(
-          left: Radius.circular(borderRadius),
+    return AspectRatio(
+      aspectRatio: 1,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.horizontal(
+            left: Radius.circular(borderRadius),
+          ),
         ),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.only(
-          left: 5,
-        ),
-        child: SfRadialGauge(
-          axes: <RadialAxis>[
-            RadialAxis(
-              startAngle: 270,
-              endAngle: 270,
-              minimum: 0,
-              maximum: maxSpots == 0 ? 1 : maxSpots,
-              showLabels: false,
-              showTicks: false,
-              axisLineStyle: const AxisLineStyle(
-                thickness: 0.15,
-                cornerStyle: CornerStyle.bothFlat,
-                color: Colors.grey,
-                thicknessUnit: GaugeSizeUnit.factor,
-              ),
-              pointers: <GaugePointer>[
-                RangePointer(
-                  value: unoccupiedLots == maxSpots ? 1 : unoccupiedLots,
-                  width: 0.15,
-                  sizeUnit: GaugeSizeUnit.factor,
-                  cornerStyle:
-                      full ? CornerStyle.bothFlat : CornerStyle.bothCurve,
-                  color: determineFreePlacesColor(garage),
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 5,
+          ),
+          child: SfRadialGauge(
+            axes: <RadialAxis>[
+              RadialAxis(
+                startAngle: 270,
+                endAngle: 270,
+                minimum: 0,
+                maximum: maxSpots == 0 ? 1 : maxSpots,
+                showLabels: false,
+                showTicks: false,
+                axisLineStyle: const AxisLineStyle(
+                  thickness: 0.15,
+                  cornerStyle: CornerStyle.bothFlat,
+                  color: Colors.grey,
+                  thicknessUnit: GaugeSizeUnit.factor,
                 ),
-              ],
-              annotations: <GaugeAnnotation>[
-                GaugeAnnotation(
-                  angle: 90,
-                  widget: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        (garage.parkingLots - garage.unoccupiedLots).toString(),
-                        style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const Text('left'),
-                    ],
+                pointers: <GaugePointer>[
+                  RangePointer(
+                    value: unoccupiedLots == maxSpots ? 1 : unoccupiedLots,
+                    width: 0.15,
+                    sizeUnit: GaugeSizeUnit.factor,
+                    cornerStyle:
+                        full ? CornerStyle.bothFlat : CornerStyle.bothCurve,
+                    color: determineFreePlacesColor(garage),
                   ),
-                )
-              ],
-            ),
-          ],
+                ],
+                annotations: <GaugeAnnotation>[
+                  GaugeAnnotation(
+                    angle: 90,
+                    widget: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          (garage.parkingLots - garage.unoccupiedLots)
+                              .toString(),
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const Text('left'),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -120,51 +123,45 @@ class GarageWidget extends StatelessWidget {
   Widget buildGarageInformationBanner(
     BuildContext context,
     Garage garage,
-    double height,
   ) {
     final Location location = garage.garageSettings.location;
-    final double width = MediaQuery.of(context).size.shortestSide;
 
-    return SizedBox(
-      height: height / 11,
-      width: width - 72,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 15,
-          vertical: 5,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              garage.name,
-              style: TextStyle(
-                color: Colors.indigo.shade800,
-                fontWeight: FontWeight.w600,
-                fontSize: 17,
-              ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 15,
+        vertical: 5,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            garage.name,
+            style: TextStyle(
+              color: Colors.indigo.shade800,
+              fontWeight: FontWeight.w600,
+              fontSize: 17,
             ),
-            const SizedBox(
-              height: 2,
+          ),
+          const SizedBox(
+            height: 2,
+          ),
+          Text(
+            location.province.toString(),
+            style: const TextStyle(
+              color: Colors.black87,
             ),
-            Text(
-              location.province.toString(),
-              style: const TextStyle(
-                color: Colors.black87,
-              ),
+          ),
+          AutoSizeText(
+            '${location.street} ${location.number}, ${location.municipality}, ${location.postCode}',
+            maxLines: 1,
+            maxFontSize: 14,
+            minFontSize: 5,
+            style: const TextStyle(
+              color: Colors.black87,
             ),
-            AutoSizeText(
-              '${location.street} ${location.number}, ${location.municipality}, ${location.postCode}',
-              maxLines: 1,
-              maxFontSize: 14,
-              minFontSize: 5,
-              style: const TextStyle(
-                color: Colors.black87,
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
