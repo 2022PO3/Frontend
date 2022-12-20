6411f531e589f326196ba698f61434fbb1ef2545
Robin Martens <robin.martens1@student.kuleuven.be>
2022-12-20 09:34:08 +0100

    Add script for release

---
+++ release.sh
@@ -0,0 +1,2 @@
+flutter build appbundle --obfuscate --split-debug-info --build-name=$1 --build-number=$2
+flutter build apk --obfuscate --split-debug-info --build-name=$1 --build-number=$2 --spit-per-abi
